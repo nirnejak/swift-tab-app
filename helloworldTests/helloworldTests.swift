9cb23bc14bf9dbdfccb6c2ef02ad887fb3045657
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2019-12-21 16:12:42 +0530

    Initial Commit

---
+++ helloworldTests/helloworldTests.swift
@@ -0,0 +1,34 @@
+//
+//  helloworldTests.swift
+//  helloworldTests
+//
+//  Created by Jitendra Nirnejak on 21/12/19.
+//  Copyright © 2019 Jitendra Nirnejak. All rights reserved.
+//
+
+import XCTest
+@testable import helloworld
+
+class helloworldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
